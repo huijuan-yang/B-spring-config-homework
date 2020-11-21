c784a763533362647f67e928cf02eed3f0e24421
Yvonne Yang _ ACI <yvonneyang_aci@Huijuans-MacBook-Pro.local>
2020-11-21 17:41:27 +0800

    p2s1 added

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,20 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
 
+	@Autowired
+	private LevelController levelController;
+
 	@Test
 	void contextLoads() {
+		assertEquals(levelController.getLevel(), "basic");
 	}
 
 }
